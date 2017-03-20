95d635c4d17ba1b82c8bc8f3f6e065e525be3248
Pesah Arthur <arthur.pesah@ensta-paristech.fr>
2017-03-21 00:15:35 +0100

    Add main2, path_glouton and compile.sh

---
+++ src/path_glouton.hpp
@@ -0,0 +1,28 @@
+#ifndef PATH_HPP_INCLUDED
+#define PATH_HPP_INCLUDED
+#include "individual.hpp"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Path_glouton : public Individual {
+private:
+    vector<int> _path;
+    int current;
+    static std::vector<std::vector<float> > _graph;
+public:
+    ~Path_glouton();
+    Path_glouton(int, bool);
+    void set_graph(vector<vector<float> >);
+    void crossOver(Individual*, Individual*);
+    void crossOver2(Individual*, Individual*);
+    void mutation();
+    void mutation2();
+    void evaluate();
+    void shuffle();
+    bool operator()(int,int);
+    friend ostream& operator<<(ostream&, Path_glouton);
+
+};
+
+#endif // PATH_HPP_INCLUDED
