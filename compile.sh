18e65e767a6261d7280adfab7cab8379853c75bf
Pesah Arthur <arthur.pesah@ensta-paristech.fr>
2017-03-21 00:17:33 +0100

    Delete compile.sh

--- compile.sh
+++
@@ -1,1 +0,0 @@
-g++ -Wall -O3 Display.cpp main.cpp AnimationState.cpp AbstractState.cpp CreateGraphState.cpp population.cpp path.cpp individual.cpp algo_genetique.cpp -std=c++11 -lsfml-graphics -lsfml-window -lsfml-system  -o traveler
